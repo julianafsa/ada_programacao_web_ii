487e6bfed1a8739305ed3ade4b75eaea05a7f8b0
Juliana Aquino <juliana.aquino@f1rst.com.br>
2023-02-12 06:39:11 -0300

    Adding script for data initialization.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,13 @@
+INSERT INTO categorias (nome) VALUES ('Biografia');
+INSERT INTO categorias (nome) VALUES ('Conto');
+INSERT INTO categorias (nome) VALUES ('Crônica');
+INSERT INTO categorias (nome) VALUES ('Ensaio');
+INSERT INTO categorias (nome) VALUES ('Ficção');
+INSERT INTO categorias (nome) VALUES ('Poesia');
+INSERT INTO categorias (nome) VALUES ('Terror');
+
+INSERT INTO editoras (nome, descricao) VALUES ('Companhia da Letras', 'Editora Companhia da Letras');
+INSERT INTO editoras (nome, descricao) VALUES ('Globo Livros', 'Editora Globo Livros');
+INSERT INTO editoras (nome, descricao) VALUES ('Darkside Books', 'Editora Darkside Books');
+INSERT INTO editoras (nome, descricao) VALUES ('Editora Rocco', 'Editora Rocco');
+INSERT INTO editoras (nome, descricao) VALUES ('Aleph', 'Editora Aleph');
